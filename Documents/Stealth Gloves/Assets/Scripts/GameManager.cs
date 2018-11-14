98b88641ecc7d3c111400829eb4867836ea41989
Leslie Xie <xie.les@northeastern.edu>
2018-11-14 13:09:52 -0500

    Scripts and Objects

---
+++ Documents/Stealth Gloves/Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static int qq = 0;
+    public static int ww = 0;
+    public static int ee = 0;
+    public static int rr = 0;
+    public static int vv = 0;
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Q))
+        {
+            qq == qq + 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            ww == ww + 1;
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ee == ee + 1;
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            rr == rr + 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            vv == vv + 1;
+        }
+    }
+}
---
+++ Documents/Stealth Gloves/Assets/Scripts/MeasureBar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeasureBar : MonoBehaviour {
+    GameObject gm = GameObject.Find("GameManager");
+
+    int measured = gm.GetComponent<GameManager>().key;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localScale = (0, measured, 0);
+	}
+}
