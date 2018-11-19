e5e0b95a4a11a449688a0f66f9102bc51a4b3451
Ian Meyers <meyers.i@husky.neu.edu>
2018-11-19 15:38:17 -0500

    Measure bar y-scale changes when pressing QWERV, decrements when keys are not being pressed.

--- Documents/Stealth Gloves/Assets/Scripts/GameManager.cs
+++ Documents/Stealth Gloves/Assets/Scripts/GameManager.cs
@@ -4,40 +4,84 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-    public static int qq = 0;
-    public static int ww = 0;
-    public static int ee = 0;
-    public static int rr = 0;
-    public static int vv = 0;
+    public int qq = 0;
+    public int ww = 0;
+    public int ee = 0;
+    public int rr = 0;
+    public int vv = 0;
+    public int key = 0;
+    public bool keyPressed = false;
 
     // Use this for initialization
     void Start () {
-        
+        InvokeRepeating("Decrement", 1.0f, 1.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.Q))
+
+        if (Input.GetKeyDown(KeyCode.Q))
         {
-            qq == qq + 1;
+            keyPressed = true;
+            qq += 1;
         }
 
         if (Input.GetKeyDown(KeyCode.W))
         {
-            ww == ww + 1;
+            keyPressed = true;
+            ww += 1;
         }
+
         if (Input.GetKeyDown(KeyCode.E))
         {
-            ee == ee + 1;
+            keyPressed = true;
+            ee += 1;
         }
+
         if (Input.GetKeyDown(KeyCode.R))
         {
-            rr == rr + 1;
+            keyPressed = true;
+            rr += 1;
         }
 
         if (Input.GetKeyDown(KeyCode.V))
         {
-            vv == vv + 1;
+            keyPressed = true;
+            vv += 1;
+        }
+
+        key = qq + ww + ee + rr + vv;
+        keyPressed = false;
+        /*qq -= 1;
+        ww -= 1;
+        ee -= 1;
+        rr -= 1;
+        vv -= 1;*/
+
+
+    }
+
+    private void Decrement()
+    {
+        if (qq > 0 && !keyPressed)
+        {
+            qq -= 1;
+        }
+        if (ww > 0 && !keyPressed)
+        {
+            ww -= 1;
+        }
+        if (ee > 0 && !keyPressed)
+        {
+            ee -= 1;
+        }
+        if (rr > 0 && !keyPressed)
+        {
+            rr -= 1;
+        }
+        if (vv > 0 && !keyPressed)
+        {
+            vv -= 1;
         }
     }
 }
--- Documents/Stealth Gloves/Assets/Scripts/MeasureBar.cs
+++ Documents/Stealth Gloves/Assets/Scripts/MeasureBar.cs
@@ -3,17 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MeasureBar : MonoBehaviour {
-    GameObject gm = GameObject.Find("GameManager");
 
-    int measured = gm.GetComponent<GameManager>().key;
+    int measured;
+    GameObject gm;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+        gm = GameObject.Find("Game Manager");
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        transform.localScale = (0, measured, 0);
+        //transform.localScale = (0, measured, 0);
+        measured = gm.GetComponent<GameManager>().key;
+        transform.localScale = new Vector3(0.5f, measured, 0.1f);
 	}
 }
